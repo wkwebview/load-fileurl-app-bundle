facbd46947eef38e9025b57192b622d18b512f11
WKWebView J Smith <mywkwebview@gmail.com>
2015-06-16 14:36:49 -0700

    Working test.

--- load-fileurl-app-bundle/load-fileurl-app-bundle/ViewController.h
+++ load-fileurl-app-bundle/load-fileurl-app-bundle/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController < WKNavigationDelegate, WKUIDelegate >
 
+@property (nonatomic, strong) WKWebView* webView;
 
 @end
 
--- load-fileurl-app-bundle/load-fileurl-app-bundle/ViewController.m
+++ load-fileurl-app-bundle/load-fileurl-app-bundle/ViewController.m
@@ -17,11 +17,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if (self.webView == nil) {
+        self.webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+        self.webView.navigationDelegate = self;
+        self.webView.UIDelegate = self;
+        [self.view addSubview:self.webView];
+    }
+    
+    NSString* indexFilePath = [self appBundleIndexFile:@"index.html" inFolder:@"www"];
+    [self.webView
+     loadFileURL:[NSURL fileURLWithPath:indexFilePath]
+     allowingReadAccessToURL:[NSURL fileURLWithPath:[indexFilePath stringByDeletingLastPathComponent]]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSString*) appBundleIndexFile:(NSString*)fileName inFolder:(NSString*)folderName
+{
+    return [[NSBundle mainBundle] pathForResource:fileName ofType:@"" inDirectory:folderName];
+}
+
+#pragma mark WKNavigationDelegate
+
+- (void)webView:(WKWebView*)webView didFailNavigation:(WKNavigation*)navigation withError:(NSError*)error
+{
+    [self webView:webView didFailProvisionalNavigation:navigation withError:error];
+}
+
+- (void)webView:(WKWebView*)webView didFailProvisionalNavigation:(WKNavigation*)navigation
+      withError:(NSError*)error
+{
+    __weak __typeof(self) weakSelf = self;
+    
+    UIAlertController* alertController = [UIAlertController  alertControllerWithTitle:@"Load Failed"  message:[error localizedDescription]  preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [weakSelf dismissViewControllerAnimated:YES completion:nil];
+    }]];
+    [weakSelf presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark WKUIDelegate
+
+- (void)webView:(WKWebView*)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo*)frame completionHandler:(void (^)(void))completionHandler
+{
+    UIAlertController* alertController = [UIAlertController alertControllerWithTitle:message
+                                                                             message:nil
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction *action) {
+                                                          completionHandler();
+                                                      }]];
+    [self presentViewController:alertController animated:YES completion:^{}];
 }
 
 @end
